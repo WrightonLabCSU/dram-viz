4f2993bef23ece4efb7e5c361da73c9bc991befa
Madeline Scyphers <madelinescyphers@gmail.com>
2024-07-29 18:19:49 -0400

    Add __version__ import to __init__.py for packaging

--- dram2_viz/__init__.py
+++ dram2_viz/__init__.py
@@ -1,2 +1,10 @@
+try:
+    from dram2_viz._version import __version__
+
+except ImportError:
+    # package not installed
+    __version__ = "0.0.0"
+
+
 __authors__ = ["Madeline Scyphers", "Rory Flynn"]
 __copyright__ = "Copyright 2024, Wrighton Lab"
